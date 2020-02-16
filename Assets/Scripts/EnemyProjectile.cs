dc7a1b5ddb5792f67ae600719615020c15c9ce10
Mohamed Morsi <mohamedashrafmorsi@gmail.com>
2020-02-16 01:16:32 -0500

    Add files via upload

---
+++ Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+
+    public float moveSpeed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = this.gameObject.GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectile.velocity = new Vector2(0, -1) * moveSpeed;
+    }
+
+    //hit detection
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SetActive(false);
+        }
+        if (col.gameObject.name == "Bottom")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MoveProjectile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour
+{
+    public Rigidbody2D projectile;//reference to a rigidbody2D
+
+    public float moveSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = this.gameObject.GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectile.velocity = new Vector2(0, 1) * moveSpeed;
+    }
+
+    //add some hit detection
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.name == "Enemy")
+        {
+            col.gameObject.SetActive(false);
+        }
+        if (col.gameObject.name == "Top")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
